37aa97a8ebe3f31faf56840daadf8ae75ed0ac85
songjaesang0518-qs <songjaesang0518@gmail.com>
2025-11-04 13:58:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
 st.title('웹 사이트 만드기')
-st.text_input('이름을 입력해주세요')
-st.button('인사말 생성')
+name=st.text_input('이름을 입력해주세요')
+st.selectbox('좋아하는 음식을 선택해주세요',['치킨','떡볶이','바나나'])
+if st.button('인사말 생성'):
+  st.wirte(name+'님 안녕하세요')
